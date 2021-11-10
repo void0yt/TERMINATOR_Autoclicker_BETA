6e2f81dca4896bf74a9538c979744643407e5d29
void0yt <93957789+void0yt@users.noreply.github.com>
2021-11-10 19:13:59 +0800

    Add files via upload

---
+++ AutoClicker_Destruction.py
@@ -0,0 +1,122 @@
+print("==================")
+print("    AutoClicker   ")
+print("==================")
+print("Created by void(0); on YouTube")
+import mouse
+import time
+import sys
+print("Loading...")
+time.sleep(5)
+loopvar = (5)
+
+
+print("Loading Done")
+var1 = input("Enable autoclicker? Y/N:  ")
+print(var1)
+if var1 == ("Y"):
+    while loopvar > 1:
+        mouse.click('left')
+    print("epic")
+
+elif var1 == ("N"):
+    sys.exit(0)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
